3e5c6bd9c7672d64e9f02c8ac3431ad9c1bc7418
WickTech <rishabhverma707@gmail.com>
2023-05-18 16:10:42 +0530

    second commit

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.get('/price', async (req, res) => {
     // Scrape the website
     const response = await axios.get('https://www.metal.com/Lithium-ion-Battery/202303240001');
     const $ = cheerio.load(response.data);
-    
+
     // Check if the price element exists
-    const priceElement = $('span.strong___1JlBD.priceDown___2TbRQ');
+    const priceElement = $('#__next > div > div.main___1ft3R.detail___2oeiJ > div.left___wCEQV > div:nth-child(3) > div.metalsContent___3T_m3 > div.priceContent___3lf_D > div > div:nth-child(1) > span.strong___1JlBD.priceDown___2TbRQ');
     if (priceElement.length === 0) {
       throw new Error('Price element not found on the website.');
-    }   
+    }
 
     // Extract the latest price
     const price = priceElement.text();
@@ -23,14 +23,15 @@ app.get('/price', async (req, res) => {
     res.json({ price });
   } catch (error) {
     // Handle any errors
+    console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching the price.' });
   }
 });
 
 app.use((req, res) => {
-    // Handle invalid routes
-    res.status(404).json({ error: 'Invalid route.' });
-  });
+  // Handle invalid routes
+  res.status(404).json({ error: 'Invalid route.' });
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
